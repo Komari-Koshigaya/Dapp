65a375de21f796108dbcf860e31bb007d5261b29
niejun <2323384399@qq.com>
2020-11-09 15:44:30 +0800

    test: 💍 Adding test about sending transactions and watching event

--- fair-vote/frontend/test/testContractCon.js
+++ fair-vote/frontend/test/testContractCon.js
@@ -85,96 +85,61 @@ let abi = [
 let VOTE_ADDRESS = "0x5D00253923cD91FFb3822Bba2FD369D35BbDD93B";
 let JSONRPC_ENDPOINT = 'http://192.168.1.102:8545';
 let JSONRPC_WS_ENDPOINT = 'ws://192.168.1.102:8545';
-// let web3 = new Web3(new Web3.providers.HttpProvider(JSONRPC_ENDPOINT));
-let web3 = new Web3(new Web3.providers.WebsocketProvider(JSONRPC_WS_ENDPOINT));
-let contractInstance = new web3.eth.Contract(abi,VOTE_ADDRESS);
-let defaultAccount;
-
-// $(document).ready(function(){
-//  if (typeof web3 !== 'undefined') {
-//         web3 = new Web3(web3.currentProvider);
-//     } else {
-//         // set the provider you want from Web3.providers 
-//         // 连接本地私有链
-//         web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-//         // web3 = new Web3(new Web3.providers.HttpProvider(JSONRPC_ENDPOINT));
-//         // web3 = new Web3(new Web3.providers.WebsocketProvider(JSONRPC_WS_ENDPOINT));
-//  }
-//     // 获取 合约对象
-//  // 
-// });
+let web3,contractInstance,defaultAccount;
 
 
+function init() {
+   //初始化Web3  连接本地私有链
+   if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        // web3 = new Web3(new Web3.providers.HttpProvider(JSONRPC_ENDPOINT));
+        web3 = new Web3(new Web3.providers.WebsocketProvider(JSONRPC_WS_ENDPOINT));
+    }
 
-function getWeb3() {
-    return web3
-}
+    // 获取 合约对象
+    contractInstance = new web3.eth.Contract(abi,VOTE_ADDRESS)
+    console.log(contractInstance);
 
-function setDefaultAccount() {
-  console.log(contractInstance)
+    //设置默认账户地址
     web3.eth.getAccounts().then((accounts) => {
       defaultAccount = accounts[0]
         console.log( "连接区块链成功，设置默认账号：" + defaultAccount );
     });
 }
-setDefaultAccount();
-
-let testContractInit = ()=>{
-  // return new Promise(async (resolve, reject) => {
- //        try {
- //            this.contractInstance.Init(123,78,112,118,119,124,129, { from: defaultAccount }, (err, transaction) => {
- //                if(!err) resolve(transaction)
- //                reject(err)
- //            })
- //        } catch(e) {
- //            reject(e)
- //        }
- //    });
+init();
 
 
+let testContractInit = _=>{
+  //使用 send调用合约 会发起一笔交易并签名 因此该账户必须已解锁
   contractInstance.methods.Init(123,78,112,118,119,124,129)
-    .call({from: defaultAccount}, function(error, result) {
-        if(!error)
-        {
-            console.log(result);
+  // .send({from: defaultAccount}) 
+  //Error: No "from" address specified in neither the given options, nor the default options.
+  //上述错误是由于defaultAccount 为空，演示可用某个固定的账户地址
+  .send({from: '0x586c9d66cDf6274a7ea9F4CAe0bdB2087cB929C7'})
+  .on('receipt', function(receipt){
+      console.log(receipt)
+      //交易发送成功后，获取产生的event事件
+      let returnValues = receipt.events.NewInit.returnValues;
+      console.log(`触发了事件，返回 new_p-${returnValues.new_p}、new_g-${returnValues.new_g}、new_q-${returnValues.new_q}、new_s-${returnValues.new_s}、new_o-${returnValues.new_o}`);
+  })
+  .on('error', function(error) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+      console.warn(error)
+  });
 
-        }
-        else console.log(error);
-    });
-
-
-  // //HttpProvider
- //    contractInstance.getPastEvents('NewInit',(function(error, result) {
- //        if (!error){
- //            console.log(result);
- //        } 
- //     else console.log(error);
- //    }));
-
-    //ws socket 
-  console.log(75);    
+  //ws socket 监听指定的event事件
   // contractInstance.events.NewInit({
   //   filter: {},
   //   fromBlock: 0
-  // }, function(error, event){
+  // },function(error, event){
   // }).on('data', function(event){
   //   console.log(event); // same results as the optional callback above
+  //   let returnValues = event.returnValues;
+  //   // 模板字符串`你好, ${name}, 你今年${age}岁了!`;
+  //   console.log(`触发了 ${event.event} 事件，返回 new_p-${returnValues.new_p}、new_g-${returnValues.new_g}、new_q-${returnValues.new_q}、new_s-${returnValues.new_s}、new_o-${returnValues.new_o}`);
   // }).on('changed', function(event){
   // // remove event from local database
   // }).on('error', console.error);
-
-  contractInstance.events.NewInit({
-    filter: {},
-    fromBlock: 0
-  },function(error, event){
-  }).on('data', function(event){
-    console.log(event); // same results as the optional callback above
-    let returnValues = event.returnValues;
-    // 模板字符串`你好, ${name}, 你今年${age}岁了!`;
-    console.log(`触发了 ${event.event} 事件，返回 new_p-${returnValues.new_p}、new_g-${returnValues.new_g}、new_q-${returnValues.new_q}、new_s-${returnValues.new_s}、new_o-${returnValues.new_o}`);
-  }).on('changed', function(event){
-  // remove event from local database
-  }).on('error', console.error);
 };
 testContractInit();
 
