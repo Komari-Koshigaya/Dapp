e209fe55b6d65d8f7fce56c63fd4ba061550f500
niejun <2323384399@qq.com>
2020-12-03 12:46:25 -0500

    feat: 🎸 complete the step2 encrypting vote in Voting phrase

--- fair-vote/frontend/purehtml/js/voteAttendEventHandler.js
+++ fair-vote/frontend/purehtml/js/voteAttendEventHandler.js
@@ -2,6 +2,7 @@ var attendVote = null; //默认投票地址
 var zeroProof = null; //证明
 
 
+//search vote before voting
 function handleSearchVote() {
     setVoteName('请先搜索您想参加的投票!');
     attendVote = null; //每次点击搜索都重置默认的投票地址
@@ -42,7 +43,7 @@ function handleSearchVote() {
 
         //设置获取到的投票相关信息
         setVoteName(`您正在参加的投票： ${attendVote.name}`);
-        if (leftOption == 1) setVoteCandidate();
+        // if (leftOption == 1) setVoteCandidate();
         parent.setVoteInfo(attendVote.vote);
         return voteList;
     }
@@ -51,11 +52,47 @@ function handleSearchVote() {
     console.log("搜索投票结束!");
 }
 
-function handleGeneProof() {
+//common check param
+function checkParaBeforeGeneProof() {
     zeroProof = null;
-    document.getElementById('voteXi').value = '61';
-    popBox();
-    let xi = +document.getElementById('voteXi').value;
+    if (Util.isEmpty(attendVote)) {
+        alert('请先确认一个投票！')
+        return -1;
+    }
+
+    let xi = prompt('请输入投票的私钥: ');
+    if (xi === null) { //用户点击取消
+        return -1;
+    }
+    if (Util.isEmpty(xi) || isNaN(xi)) {
+        alert('请输入整数！')
+        return -1;
+    }
+    xi = Number(xi);
+    return xi;
+}
+
+function checkParaBeforeSendChain() {
+    if (Util.isEmpty(attendVote)) {
+        alert('请先确认一个投票！')
+        return false;
+    }
+    if (Util.isEmpty(zeroProof)) {
+        alert('请先生成证明！')
+        return false;
+    }
+    if (Util.isEmpty(parent.currAccount)) {
+        alert('请先解锁账户');
+        return false;
+    }
+    return true;
+}
+
+
+//step1 register
+function handleGeneRegisterProof() {
+    let xi = checkParaBeforeGeneProof();
+    if (xi === -1) return false;
 
     let main = async () => {
         zeroProof = await Vote.geneRegProof(attendVote.vote, xi);
@@ -74,49 +111,77 @@ function handleGeneProof() {
 }
 
 function handleRegisterVote() {
-    if(Util.isEmpty(attendVote)){
-        alert('请先确认一个投票！')
-        return false;
-    }
-    if(Util.isEmpty(zeroProof)){
-        alert('请先生成证明！')
-        return false;
-    }
-    if (Util.isEmpty(parent.currAccount)) {
-        alert('请先解锁账户');
-        return false;
-    }
+    if (checkParaBeforeSendChain() === false) return;
 
     Vote.registerToVote(attendVote.vote, zeroProof.r, zeroProof.a, zeroProof.c, zeroProof.yi)
         .then()
         .catch(console.log);
 }
 
-function handleEncryptVote(vote_addr) {
-    //例子2
-    let result = prompt("请输入私钥和候选人编号(','分隔;编号从0开始)", "61, 1");
+//step2 encrypt
+function handleGeneEncryptProof() {
+    zeroProof = null;
+    if (Util.isEmpty(attendVote)) {
+        alert('请先确认一个投票！')
+        return -1;
+    }
+
+    let result = prompt("请输入私钥和候选人编号,如: 61,1");
     result = result.split(",");
-    let xi = result[0],
-        voteNum = Number(result[1]);
-    Vote.encryptToVote(test_address, voteNum, xi);
+    let xi = Number(result[0]), voteNum = Number(result[1]);
+
+
+    let main = async () => {
+        zeroProof = await Vote.geneEncProof(attendVote.vote, xi, voteNum);
+
+        if (Util.isEmpty(zeroProof)) {
+            alert('自动生成出错！')
+            return;
+        }
+        console.log(zeroProof);
+        document.getElementById('EncryptVote').value = zeroProof.vencVi;
+        document.getElementById('EncryptProof').value = `ajList [${zeroProof.ajList}], bjList [${zeroProof.bjList}], rjList [${zeroProof.rjList}], djList [${zeroProof.djList}], c ${zeroProof.c}, vencVi ${zeroProof.vencVi}, Yi ${zeroProof.Yi}, currVoterYi ${zeroProof.currVoterYi}`;
+    }
+
+    main();
 }
 
+function handleEncryptVote(vote_addr) {
+    if (checkParaBeforeSendChain() === false) return;
+
+    Vote.encryptToVote(attendVote.vote, zeroProof.ajList, zeroProof.bjList, zeroProof.rjList, zeroProof.djList, zeroProof.c, zeroProof.vencVi, zeroProof.Yi, zeroProof.currVoterYi)
+        .then(()=>confirm("加密投票成功!"))
+        .catch(console.log);
+}
+
+//step3 decrypt
 function handleDecryptVote(vote_addr, xi) {
     Vote.decryptToVote(test_address, test_xi);
 }
 
+//step4 assist
 function handleConstructVote(vote_addr, xi) {
     Vote.constructVote(test_address, test_xi);
 }
 
+//step5 recover
 function handleRecoverVote(vote_addr, xi) {
     Vote.recoverVote(test_address, test_xi);
 }
 
+//tally vote result phrase
 function handleTallyVote(vote_addr) {
     Vote.tallyVote(test_address);
 }
 
+
+
+
+
+
+/**
+ *  
+ */
 var leftOption = 0;
 //根据左侧按钮切换界面
 function LeftOption(index) {
@@ -173,7 +238,6 @@ function closeBox() {
     popBox.style.display = "none";
     popLayer.style.display = "none";
 }
-
 var radioOption = -1;
 
 function radio(index) {
@@ -184,7 +248,6 @@ function radio(index) {
     });
     radios[index].setAttribute("class", "btnRadioClick");
 }
-
 
 function setVoteName(name) {
     var names = document.getElementsByName("voteName");
@@ -237,52 +300,51 @@ function SetRadioOption(nameList) {
     };
 }
 
-
-function Vote() {
-    if (voteAddress == undefined || radioOption == -1) return;
-
-    var web3 = parent.web3;
-    var abi = parent.voteAbi;
-    var account = parent.currAccount;
-    if (account == undefined) return;
-
-    var voteContract = new web3.eth.Contract(abi, voteAddress);
-    voteContract.methods.GetVoteInfo().call(function(error, info) {
-        if (error) {
-            console.log(error);
-        }
-
-        if (info[3] * 1000 > new Date().getTime()) {
-            alert(`投票尚未开始，请稍等！\n开始时间：${ParseTimeFormat(info[3] * 1000," - ")}\n结束时间：${ParseTimeFormat(info[4] * 1000, " - ")}`)
-            return;
-        }
-        if (info[4] * 1000 < new Date().getTime()) {
-            alert(`投票已经结束！\n开始时间：${ParseTimeFormat(info[3] * 1000," - ")}\n结束时间：${ParseTimeFormat(info[4] * 1000, " - ")}`)
-            return;
-        }
-        if (parent.currPwd == undefined || !parent.currAccount) {
-            alert("账号未解锁或账号不存在!");
-            return;
-        }
-        web3.eth.personal.unlockAccount(parent.currAccount, parent.currPwd, (error, result) => {
-            if (error) {
-                alert("账号出错");
-            } else {
-                voteContract.methods.ToVote(radioOption).send({ from: account, password: parent.currPwd })
-                    .on('receipt', function(receipt) {
-                        if (receipt.events.Success.returnValues[0])
-                            alert("投票成功");
-                    })
-                    .on('error', function(error) {
-                        alert('投票出错')
-                    })
-            }
-        })
-    })
-
-}
-
 function SearchBaseChange(index) {
     var text = index == 0 ? "请输入投票地址..." : "请输入投票名称...";
     document.getElementById("SearchText").setAttribute("placeholder", text);
 }
+
+// function Vote() {
+//     if (voteAddress == undefined || radioOption == -1) return;
+
+//     var web3 = parent.web3;
+//     var abi = parent.voteAbi;
+//     var account = parent.currAccount;
+//     if (account == undefined) return;
+
+//     var voteContract = new web3.eth.Contract(abi, voteAddress);
+//     voteContract.methods.GetVoteInfo().call(function(error, info) {
+//         if (error) {
+//             console.log(error);
+//         }
+
+//         if (info[3] * 1000 > new Date().getTime()) {
+//             alert(`投票尚未开始，请稍等！\n开始时间：${ParseTimeFormat(info[3] * 1000," - ")}\n结束时间：${ParseTimeFormat(info[4] * 1000, " - ")}`)
+//             return;
+//         }
+//         if (info[4] * 1000 < new Date().getTime()) {
+//             alert(`投票已经结束！\n开始时间：${ParseTimeFormat(info[3] * 1000," - ")}\n结束时间：${ParseTimeFormat(info[4] * 1000, " - ")}`)
+//             return;
+//         }
+//         if (parent.currPwd == undefined || !parent.currAccount) {
+//             alert("账号未解锁或账号不存在!");
+//             return;
+//         }
+//         web3.eth.personal.unlockAccount(parent.currAccount, parent.currPwd, (error, result) => {
+//             if (error) {
+//                 alert("账号出错");
+//             } else {
+//                 voteContract.methods.ToVote(radioOption).send({ from: account, password: parent.currPwd })
+//                     .on('receipt', function(receipt) {
+//                         if (receipt.events.Success.returnValues[0])
+//                             alert("投票成功");
+//                     })
+//                     .on('error', function(error) {
+//                         alert('投票出错')
+//                     })
+//             }
+//         })
+//     })
+
+// }
